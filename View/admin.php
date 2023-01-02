76767d2172ae9b4b5476ed041878ebfc9b08c571
Leonel <leonel.diniz@live.com>
2023-01-02 18:33:38 -0300

    update

--- View/admin.php
+++ View/admin.php
@@ -30,7 +30,7 @@ $total2 = mysqli_num_rows($dados2);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lista Gira</title>
+    <title>Lista Gira - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
